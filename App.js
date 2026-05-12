b7097ad11a4d34f8b1e83680cfe85ab031a4cedc
ungiosbeem95-png <ungiosbeem95@gmail.com>
2026-05-12 18:41:23 +0300

    Create App component with welcome message
    Initial implementation of the Chat App with a welcome message.

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Ku soo dhawaada Chat App-ka Abdalla Keynan!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
